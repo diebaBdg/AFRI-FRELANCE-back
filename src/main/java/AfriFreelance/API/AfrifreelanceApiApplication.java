7ccfc5c5ff4211e534f4fd7952dd3d334719076d
Dieynaba Fily  Bodiang <dfbodiang@gainde2000.sn>
2026-08-21 19:00:22 +0000

    first commit

---
+++ src/main/java/AfriFreelance/API/AfrifreelanceApiApplication.java
@@ -0,0 +1,13 @@
+package AfriFreelance.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfrifreelanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfrifreelanceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AfriFreelance/API/AfrifreelanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package AfriFreelance.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfrifreelanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
