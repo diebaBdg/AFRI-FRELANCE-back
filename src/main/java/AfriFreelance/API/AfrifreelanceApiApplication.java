f47045114da79001558f646a471fd083f96d8234
Dieynaba Fily  Bodiang <dfbodiang@gainde2000.sn>
2026-08-27 13:29:36 +0000

    some fix

--- src/main/java/AfriFreelance/API/AfrifreelanceApiApplication.java
+++ src/main/java/AfriFreelance/API/AfrifreelanceApiApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AfrifreelanceApiApplication {
+public class  AfrifreelanceApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AfrifreelanceApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AfrifreelanceApiApplication.class, args);
+        displayStartupInfo();
+    }
 
+    private static void displayStartupInfo() {
+        System.out.println("\n" +
+                "Afri-freelance Backend API démarrée !\n" +
+                "API Docs: http://localhost:8080/api/swagger-ui.html");
+    }
 }
